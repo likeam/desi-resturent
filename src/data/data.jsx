2254d5938eb733e960bf540dcf76198ce2465ef9
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-02-03 10:58:06 +0500

    Buildin Menu Section

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import Chola from '../images/chhola.jpg';
+import Dosa from '../images/dosa.jpg';
+import Gujrati from '../images/gujrati.jpeg';
+import Idli from '../images/idli.jpg';
+import MasalaDosa from '../images/masala.jpg';
+import Paneer from '../images/paneer.jpg';
 
-function data() {
-  return (
-    <div>data</div>
-  )
-}
-
-export default data
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: 200,
+  },
+  {
+    name: "Chola",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chola,
+    price: 250,
+  },
+  {
+    name: "Idli Sambhar",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Idli,
+    price: 300,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Paneer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: "Gujrati",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Gujrati,
+    price: 500,
+  },
+];
