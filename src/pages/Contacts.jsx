7f6d4592e6a1e8a66bec39478edc78530593a8de
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-02-03 08:36:02 +0500

    Building Contact section

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Layout from '../components/Layout'
 import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import EmailIcon from '@mui/icons-material/Email';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 function Contacts() {
   return (
@@ -13,7 +15,8 @@ function Contacts() {
         '& h4':{
           fontWeight : 'bold',
           mb: 2,
-        }
+        },
+       
       }}>
         <Typography variant='h4'>
           Contacts
@@ -21,20 +24,30 @@ function Contacts() {
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, fuga, beatae corrupti nisi repellat consectetur, debitis deleniti rerum veniam ab libero sit nostrum accusantium recusandae! Sint fuga ex iste placeat.</p>
       </Box>
       <Box>
-        <TableContainer component={Paper} >
-          <Table aria-label= "contact table">
-            <TableHead>
+        <TableContainer component={Paper} sx={{backgroundColor: 'lightslategray'}} >
+          <Table aria-label= "contact table" >
+            <TableHead > 
               <TableRow>
-                <TableCell>
+                <TableCell align='center'>
                   Contact Detail
                 </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               <TableRow>
-                <TableCell>
-                  <SupportAgentIcon />
+                <TableCell align='center'>
+                  <SupportAgentIcon sx={{color: 'red', pt: 1}} />  111-222-333 (Toll Free)
                 </TableCell>
+                </TableRow>
+                <TableRow>
+                <TableCell align='center'>
+                  <EmailIcon sx={{color: 'blue', pt: 1}} />  Support@gmail.com
+                </TableCell>
+                </TableRow>
+                <TableRow>
+                <TableCell align='center'>
+                  <WhatsAppIcon sx={{color: 'green', pt: 1}} />  03006644777
+                </TableCell>                 
               </TableRow>
             </TableBody>
           </Table>
